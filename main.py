d15a7c798a0ceff4a650a9eb98a7cf20dadfb2d6
Violeta Kalcheva <violetakalcheva@abv.bg>
2022-06-23 15:18:07 +0300

    add validations

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import enum
 
 from decouple import config
-from flask import Flask
+from flask import Flask, request
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+from password_strength import PasswordPolicy
+from marshmallow import Schema, fields, ValidationError, validate
 from sqlalchemy import func
 
 
@@ -15,6 +17,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{config("DB_USER")}:{conf
 db = SQLAlchemy(app)
 api = Api(app)
 migrate = Migrate(app, db)
+
+
+def validate_name(name):
+    try:
+        first_name, last_name = name.split()
+    except ValueError:
+        raise ValidationError('Full name is required!')
+
+
+policy = PasswordPolicy.from_names(
+    uppercase=1,  # need min. 1 uppercase letters
+    numbers=1,  # need min. 1 digits
+    special=1,  # need min. 1 special characters
+    nonletters=1,  # need min. 1 non-letter characters (digits, specials, anything)
+)
+
+
+def validate_password(value):
+    errors = policy.test(value)
+    if errors:
+        raise ValidationError(f"Not a valid password")
+
+
+class BaseUserSchema(Schema):
+    email = fields.Email(required=True)
+    full_name = fields.Str(required=True, validate=validate.And(validate_name, validate.Length(min=3, max=255)))
+
+
+class UserSignInSchema(BaseUserSchema):
+    password = fields.Str(required=True, validate=validate.And(validate_password, validate.Length(min=8, max=20)))
 
 
 class User(db.Model):
@@ -60,6 +92,19 @@ class Clothes(db.Model):
     create_on = db.Column(db.DateTime, server_default=func.now())
     updated_on = db.Column(db.DateTime, onupdate=func.now())
 
+
+class SignUp(Resource):
+    def post(self):
+        data = request.get_json()
+        schema = UserSignInSchema()
+        errors = schema.validate(data)
+        if not errors:
+            db.session.add(User(**data))
+            db.session.commit()
+        return errors
+
+
+api.add_resource(SignUp, "/register")
 
 if __name__ == "__main__":
     app.run(debug=True)
